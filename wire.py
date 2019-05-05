5a7990e07c15755e9bc8900da65011d4bc271543
JVFL <jvitor99.lima@gmail.com>
2019-05-05 18:19:16 +0000

    versao5

--- wire.py
+++ wire.py
@@ -2,8 +2,10 @@ import socket
 
 host  = '127.0.0.1'
 
-s = socket.socket(socket.AF_PACKET,socket.SOCK_RAW,socket.ntohs(0x0800))
+s = socket.socket(socket.AF_INET,socket.SOCK_RAW,socket.ntohs(0x0800))
+print("")
 
 while True:
 	pacote  =  s.recvfrom(65565)
 	print (pacote)
+	
