023302c1b572316a3b9a2720de8142ef0b9c9756
gresendesa <gresendesa@gmail.com>
2019-05-10 15:11:44 -0300

    Comments in portuguese on link_level

--- link_level.py
+++ link_level.py
@@ -1,13 +1,16 @@
 import socket
 import struct
 
+#Classe que representa o socket configurado para capturar pacotes de baixo nível
 class RawSocket:
 	def __init__(self):
 		self.conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
 
+	#Extrai bufsize bytes do socket
 	def read(self):
-		return self.conn.recvfrom(65536)
+		return self.conn.recvfrom(bufsize=65536)
 
+#Classe que representa a camada de enlace
 class Ethernet:
 	def __init__(self, raw_socket):
 		self.raw_socket = raw_socket
@@ -24,14 +27,17 @@ class Ethernet:
 		dest_mac, src_mac, type = struct.unpack('! 6s 6s H', self.data[:14])
 		return Ethernet.Frame(self.stringify_mac_addr(dest_mac), self.stringify_mac_addr(src_mac), socket.htons(type), self.data[14:])
 
+	#Implementa um gerador para ser iterado no for
 	def frames(self):
 		while 1:
 			yield self.get_frame()
 
+	#Torna  mac de bytes para string
 	def stringify_mac_addr(self, bytes_addr):
 		str_addr = map('{:02x}'.format, bytes_addr)
 		return ':'.join(str_addr).upper()
 
+	#Classe que representa um quadro Ethernet (da camada de enlace)
 	class Frame:
 		def __init__(self, dest, src, type, data):
 			self.dest = dest
