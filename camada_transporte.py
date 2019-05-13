5452959819fdc136f4c33af3c4632fd2a01088e8
gresendesa <gresendesa@gmail.com>
2019-05-13 01:19:25 -0300

    transport

--- camada_transporte.py
+++ camada_transporte.py
@@ -1,13 +1,13 @@
 import struct
 
 #Ver também página 141 do Kurose
-#ver https://docs.python.org/2/library/struct.html para entender struct.unpack
-#ver https://docs.python.org/3/library/stdtypes.html para entender operação envolvendo bits
+#https://docs.python.org/3/library/stdtypes.html
+#
 
 #Conforme página 148 do Kurose
 class SegmentoUDP:
 	def __init__(self, bytes_brutos):
-		self.orig_porta, self.dest_porta, self.comprimento = struct.unpack('!H H 2x H', bytes_brutos[:8])
+		self.orig_porta, self.dest_porta, self.comprimento = struct.unpack('! H H 2x H', bytes_brutos[:8])
 		self.dados = bytes_brutos[8:]
 
 #Conforme página 172 do Kurose
@@ -15,5 +15,7 @@ class SegmentoTCP:
 	def __init__(self, bytes_brutos):
 		self.orig_porta, self.dest_porta, self.sequencia, self.reconhecimento, flags = struct.unpack('! H H L L H', bytes_brutos[:14])
 		self.offset = (flags >> 12) * 4
-		self.flags = (urg, ack, psh, rst, syn, fin) = ((flags & 20), (flags & 10), (flags & 8), (flags & 4), (flags & 2), (flags & 1))
+		self.flags = (urg, ack, psh, rst, syn, fin) = (((flags & 32) >> 5), ((flags & 32) >> 4), ((flags & 32) >> 3), ((flags & 32) >> 2), ((flags & 32) >> 1), ((flags & 32) >> 1))
 		self.dados = bytes_brutos[self.offset:]
+
+
